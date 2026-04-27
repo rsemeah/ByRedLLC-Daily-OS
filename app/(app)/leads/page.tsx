1bcb3ac1116817fe02c7af7e86eedd1d410d8b02
KP <clashon64@gmail.com>
2026-04-27 15:49:04 -0700

    Revert "merge: combine local site with remote init (resolve .gitignore)"
    This reverts commit 6f983a0021075a901195eee7409106848a925e8b, reversing
    changes made to 034076bbd2cc1e72bf989ac5dede99f71b19c60c.

--- app/(app)/leads/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getLeads } from "@/lib/data/leads"
-import { LeadsKanban } from "@/components/byred/leads-kanban"
-
-export default async function LeadsPage() {
-  const leads = await getLeads()
-
-  return <LeadsKanban initialLeads={leads} />
-}
--- app/(app)/tasks/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getTasks } from "@/lib/data/tasks"
-import { TasksList } from "@/components/byred/tasks-list"
-
-export default async function TasksPage() {
-  const tasks = await getTasks()
-
-  return <TasksList initialTasks={tasks} />
-}
